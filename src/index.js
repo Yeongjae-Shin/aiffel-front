f1269ab73c971361355846f928438c3c767dce60
Yeongjae-Shin <syj9484@gmail.com>
2021-02-17 21:07:09 +0900

    [add] global style file to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import GlobalStyles from './GlobalStyles';
 import Routes from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Routes />
   </React.StrictMode>,
   document.getElementById('root'),

5a03458e4612f7f822dfeb1581250e5821fbf6c6
Yeongjae-Shin <syj9484@gmail.com>
2021-02-17 21:07:24 +0900

    [create] routes for routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import App from './App';
+import AuthCheck from './lib/hoc/AuthCheck';
+
+import Login from './components/login/Login';
+import Forum from './components/forum/forum';
+import Post from './components/post/Post';
+import Profile from './components/profile/Profile';
 
 import dotenv from 'dotenv';
 
@@ -11,7 +16,10 @@ const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/forum" component={AuthCheck(Forum, true)} />
+        <Route exact path="/forum/:id" component={AuthCheck(Post, true)} />
+        <Route exact path="/profile" component={AuthCheck(Profile, true)} />
       </Switch>
     </Router>
   );

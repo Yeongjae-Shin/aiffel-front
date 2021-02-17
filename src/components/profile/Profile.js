0c3463bda88ef50f2f33090aa7c82f4577e66af8
Yeongjae-Shin <syj9484@gmail.com>
2021-02-17 21:08:27 +0900

    [create] profile page

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../../common/Layout';
+
+const Profile = () => {
+  return (
+    <Layout>
+      <h2>프로필 페이지 입니다.</h2>
+    </Layout>
+  );
+};
+
+export default Profile;
